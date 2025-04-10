fee56df761d28aaa30979582cded7eba5e9d379f
Success <success@MacBookPro.lan>
2025-04-10 17:52:16 +0200

    Correct the file next.config.js and package.json to enable the server start

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -7,7 +7,7 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 async function seedUsers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await sql`
-    CREATE TABLE IF NOT EXISTS users (
+    CREATE TABLE IF NOT EXISTS users (git add .
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       email TEXT NOT NULL UNIQUE,
--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   // Add experimental config to resolve bcryptjs and mongoose issues
-  experimental: {
+  //experimental: {
     serverComponentsExternalPackages: ["bcryptjs", "mongoose"],
-    esmExternals: "loose",
-  },
+    //esmExternals: "loose",
+ // },
   // Optional: Add image domain config if needed (already in your code)
   images: {
     domains: ["lh3.googleusercontent.com"],
