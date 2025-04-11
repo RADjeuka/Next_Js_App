d809579e72b5401691b406a56612c79b00c6ac04
Success <success@MacBookPro.lan>
2025-04-11 22:28:27 +0200

    Chapter 13 Complete: handle errors gracefully in your application

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,5 @@
 'use server';
- 
+
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
@@ -7,13 +7,13 @@ import postgres from 'postgres';
 
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
- 
+
 const FormSchema = z.object({
-  id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
-  date: z.string(),
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
@@ -24,42 +24,53 @@ export async function createInvoice(formData: FormData) {
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-      });
-      // Test it out:
-      //console.log(rawFormData);
-      const amountInCents = amount * 100;
-      //const date = new Date().toISOString().split('T')[0];
- 
-  await sql`
-     UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+    });
+    // Test it out:
+    //console.log(rawFormData);
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    try {
+        await sql`
+          INSERT INTO invoices (customer_id, amount, status, date)
+          VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    } catch (error) {
+        // We'll log the error to the console for now
+        console.error(error);
+    }
 
     revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
     const { customerId, amount, status } = UpdateInvoice.parse({
-      customerId: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
     });
-   
+
     const amountInCents = amount * 100;
-   
-    await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
-   
+
+    try {
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+          `;
+    } catch (error) {
+        // We'll log the error to the console for now
+        console.error(error);
+    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
-  }
+}
 
-  export async function deleteInvoice(id: string) {
+export async function deleteInvoice(id: string) {
+    throw new Error('Failed to Delete Invoice');
+
+    // Unreachable code block
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
-  }
+}
